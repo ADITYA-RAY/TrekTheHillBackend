413442fb4505d568742826d48c064d45cbb38466
Aditya Ray <74045024+ADITYA-RAY@users.noreply.github.com>
2022-02-04 16:18:25 +0530

    Revert "sponsers api endpoint creadted"

--- sponser/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Sponser
-
-
-class SponserAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category')
-
-
-admin.site.register(Sponser, SponserAdmin)
--- sponser/serializers.py
+++
@@ -1,9 +0,0 @@
-
-from rest_framework import serializers
-from .models import Sponser
-
-
-class SponserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Sponser
-        fields = ['name','link','image','category']
